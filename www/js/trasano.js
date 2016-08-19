33cdfb511b9d9b113b6e8a351caed73b748f2079
raulvall <raul.vallinot@gmail.com>
2016-08-19 21:44:12 +0200

    Add files via upload

--- www/js/trasano.js
+++ www/js/trasano.js
@@ -20,18 +20,21 @@ function getDNILetter ()
 function saveDNI () 
 { 
     localStorage.setItem("dni", $("#dniNumber").val());
-    //alert("Paciente registrado!"); 
-    navigator.notification.alert("DNI: " +  $("#dniNumber").val(), function() {}, "Registrado");
+    alert("Paciente registrado!"); 
+    //navigator.notification.alert("DNI: " +  $("#dniNumber").val(), function() {}, "Registrado");
+    //navigator.notification.vibrate(500);
     window.location = "../index.html";                                 
 }
 
 function deleteDNI(){
     localStorage.removeItem("dni");
+    //navigator.notification.vibrate(500);
     window.location = "registeredUser.html";               
 }
 
 function modifyDNI(){
-    window.location = "modifyUser.html";             
+    window.location = "modifyUser.html";
+    //navigator.notification.vibrate(500);             
 }
 
 /*************************************************************************************************************
@@ -106,17 +109,24 @@ function cancelAmbulance()
 
 
 /*************************************************************************************************************
-* Source: close.html
+* initMimeTypeListener
 **************************************************************************************************************/
 function onNdef (nfcEvent) {
-    console.log(JSON.stringify(nfcEvent.tag));
-    navigator.notification.alert(nfc.bytesToHexString(nfcEvent.tag.id), function() {}, "NFC Tag ID");    
+    // Get tag ID
+    console.log("trasano.onNdef: " + JSON.stringify(nfcEvent.tag));
+    navigator.notification.alert(nfc.bytesToHexString(nfcEvent.tag.id), function() {}, "NFC Tag ID");        
     
+    // Get tag message
     var payload = nfcEvent.tag.ndefMessage[0]["payload"];
+    console.log("trasano.onNdef.ndefMessage.payload: " + nfc.bytesToString(payload));
     navigator.notification.alert(nfc.bytesToString(payload), function() {}, "NdefMessage payload");
     
-    //var tagId = nfc.bytesToHexString(tag.id);                    
-    //alert(tagId);
+    // PRODUCCION: sessionStorage()
+    // Set Ambulance
+    var date = new Date();
+    var ambulance = {"time" : date.toString(), "location" : nfc.bytesToString(payload)};
+    localStorage.setItem("ambulance", JSON.stringfy(ambulance)):
+    console.log("trasano.onNdef.localStorage.ambulance: " + JSON.stringfy(ambulance));
     navigator.vibrate(500);        
 }
 
@@ -132,7 +142,10 @@ function failMime(reason) {
 function initMimeTypeListener() {    
     nfc.addMimeTypeListener("text/plain", onNdef, winMime, failMime);              
 }
-/*---------------------------------------------------------------------------------------*/
+
+/*************************************************************************************************************
+* discoverTag
+**************************************************************************************************************/
 function nfcListener(nfcEvent) {
     navigator.notification.alert(nfc.bytesToHexString(nfcEvent.tag.id), function() {}, "NFC Tag ID");    
     navigator.notification.alert(nfc.bytesToHexString(nfcEvent.type), function() {}, "NFC Tag Type");
@@ -149,7 +162,10 @@ function failDiscoverTag(reason) {
 function discoverTag () {
      nfc.addTagDiscoveredListener(nfcListener, winDiscoverTag, failDiscoverTag);    
 }
-/*---------------------------------------------------------------------------------------*/
+
+/*************************************************************************************************************
+* initNdefListener
+**************************************************************************************************************/
 function ndefListener(nfcEvent) {
     console.log(JSON.stringify(nfcEvent.tag));
     navigator.notification.vibrate(500);
@@ -166,4 +182,3 @@ function failNdef(reason) {
 function initNdefListener () {
      nfc.addNdefListener(ndefListener, winNdef, failNdef);    
 }
-/*---------------------------------------------------------------------------------------*/
